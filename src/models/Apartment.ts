ad41663ed35c58c280a89a33044f8d64f7b6ca0b
dmmaia <david_mmaial.hotmail.com>
2020-08-24 18:11:21 -0300

    Starting to use uuidv4

--- src/models/Apartment.ts
+++ src/models/Apartment.ts
@@ -1,14 +1,19 @@
+import { uuid } from "uuidv4";
+
 interface Occupation{
   numberOfAdults: Number;
   numberOfChildren: Number;
 }
 
 class Apartment{
+  _id: String;
+
   name: String;
 
   maxOccupation: Occupation;
 
-  constructor({name, maxOccupation}: Apartment){
+  constructor({name, maxOccupation}: Omit<Apartment, '_id'>){
+    this._id = uuid();
     this.name = name;
     this.maxOccupation = maxOccupation;
   }
--- src/models/Product.ts
+++ src/models/Product.ts
@@ -1,11 +1,15 @@
+import { uuid } from "uuidv4";
+
 class Product {
+  _id: String;
+
   name:  String;
 
   price: Number;
 
-  constructor({name, price}: Product){
+  constructor({name, price}: Omit<Product, '_id'>){
+    this._id = uuid();
     this.name = name;
-
     this.price = price;
   }
 }
